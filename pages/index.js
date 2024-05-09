e214ccf23628bd2c3b949e355b2a09766a640831
Gabriel-Schiestl <gabriel.schiestl25@hotmail.com>
2024-05-09 17:24:05 -0300

    V2

--- pages/index.js
+++ pages/index.js
@@ -27,31 +27,41 @@ export default function Login() {
     }
 
     return (
-        <div className={styles.container}>
-            <header className={styles.header}></header>
-            <div className={styles.div}>
-            <h1 style={{ fontFamily: 'Inter', fontSize: '38.32px', fontWeight: 'bold', paddingTop: '7%', marginLeft: '8.5vw' }}>Iniciar sessão na VerdeCard</h1>
-            <form className={styles.form} onSubmit={handleSubmit}>
-                <div className={styles.user}>
-                    <label>Nome de usuário</label>
-                    <input type="email" value={data.userName} onChange={(e) => { setData({ ...data, userName: e.target.value }) }}></input>
+        <>
+            <div className={styles.container}>
+                <header className={styles.header}></header>
+                <div className={styles.div}>
+                    <h1 className={styles.h1}>Iniciar sessão na VerdeCard</h1>
+                    <form className={styles.form} onSubmit={handleSubmit}>
+                        <div className={styles.user}>
+                            <label>Nome de usuário</label>
+                            <input type="email" value={data.userName} onChange={(e) => { setData({ ...data, userName: e.target.value }) }}></input>
+                        </div>
+                        {errors.userNameError && <div className={styles.userNameFlash}>{errors.userNameError}</div>}
+                        <div className={styles.password}>
+                            <label>Senha</label>
+                            <input type="password" value={data.password} onChange={(e) => { setData({ ...data, password: e.target.value }) }}></input>
+                        </div>
+                        {errors.passwordError && <div className={styles.passwordFlash}>{errors.passwordError}</div>}
+                        <div className={styles.keepConn}>
+                            <input type='checkbox'></input>
+                            <label>Manter-se conectado</label>
+                        </div>
+                        <button onClick={handleSubmit}>Login</button>
+                        <Link className={styles.link} href='/forgotPassword'>Esqueceu a senha?</Link>
+                    </form>
                 </div>
-                {errors.userNameError && <div className={styles.userNameFlash}>{errors.userNameError}</div>}
-                <div className={styles.password}>
-                    <label>Senha</label>
-                    <input type="password" value={data.password} onChange={(e) => { setData({ ...data, password: e.target.value }) }}></input>
-                </div>
-                {errors.passwordError && <div className={styles.passwordFlash}>{errors.passwordError}</div>}
-                <div className={styles.keepConn}>
-                    <input type='checkbox'></input>
-                    <label>Manter-se conectado</label>
-                </div>
-                <button onClick={handleSubmit}>Login</button>
-                <Link className={styles.link} href='/forgotPassword'>Esqueceu a senha?</Link>
-            </form>
+                <footer className={styles.footer}></footer>
             </div>
-            <Image className={styles.image} src='/michel.png' width={300} height={300}></Image>
-            <footer className={styles.footer}></footer>
-        </div>
+            <div className={styles.image}>
+                <Image src='/michel_card.png' width={700} height={500}></Image>
+            </div>
+            <div className={styles.logo}>
+                <Image src='https://lojaqueroquero.vtexassets.com/assets/vtex.file-manager-graphql/images/9ab2d4be-0913-4a93-bb23-0f407b34324d___95a0c9e10947f4f06c72dcbdad1cd104.svg' 
+                width={500} 
+                height={300}>
+                </Image>
+            </div>
+        </>
     )
 }
