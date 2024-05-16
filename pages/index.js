db71075755b4d5c1e84fb338ebe81b11ac95dcb0
Gabriel-Schiestl <gabriel.schiestl25@hotmail.com>
2024-05-16 17:44:59 -0300

    Imagens

--- pages/index.js
+++ pages/index.js
@@ -30,19 +30,15 @@ export default function Login() {
         <>
             <div className={styles.container}>
                 <header className={styles.header}></header>
-                <div className={styles.div}>
+                <div className={styles.content}>
                     <h1 className={styles.h1}>Iniciar sessão na VerdeCard</h1>
                     <form className={styles.form} onSubmit={handleSubmit}>
-                        <div className={styles.user}>
-                            <label>Nome de usuário</label>
-                            <input type="email" value={data.userName} onChange={(e) => { setData({ ...data, userName: e.target.value }) }}>
-                            </input>
-                        </div>
+                        <label>Nome de usuário</label>
+                        <input type="email" value={data.userName} onChange={(e) => { setData({ ...data, userName: e.target.value }) }}>
+                        </input>
                         {errors.userNameError && <div className={styles.userNameFlash}>{errors.userNameError}</div>}
-                        <div className={styles.password}>
-                            <label>Senha</label>
-                            <input type="password" value={data.password} onChange={(e) => { setData({ ...data, password: e.target.value }) }}></input>
-                        </div>
+                        <label>Senha</label>
+                        <input type="password" value={data.password} onChange={(e) => { setData({ ...data, password: e.target.value }) }}></input>
                         {errors.passwordError && <div className={styles.passwordFlash}>{errors.passwordError}</div>}
                         <div className={styles.keepConn}>
                             <input type='checkbox'></input>
@@ -52,21 +48,23 @@ export default function Login() {
                         <Link className={styles.link} href='/recoveryPassword'>Esqueceu a senha?</Link>
                     </form>
                 </div>
+                <div className={styles.images}>
+                    <div className={styles.logoContainer}>
+                        <Image className={styles.logo} src='https://lojaqueroquero.vtexassets.com/assets/vtex.file-manager-graphql/images/9ab2d4be-0913-4a93-bb23-0f407b34324d___95a0c9e10947f4f06c72dcbdad1cd104.svg'
+                            layout='intrinsic'
+                            width={600}
+                            height={400}>
+                        </Image>
+                    </div>
+                    <div className={styles.michel}>
+                        <Image className={styles.logo} src='/michel_card.png'
+                            layout='intrinsic'
+                            width={700}
+                            height={500}>
+                        </Image>
+                    </div>
+                </div>
                 <footer className={styles.footer}></footer>
-            </div>
-            <div className={styles.image}>
-                <Image className={styles.img} src='/michel_card.png' 
-                layout='responsive'
-                width={700}
-                height={500}>
-                </Image>
-            </div>
-            <div className={styles.logoContainer}>
-                <Image className={styles.logo} src='https://lojaqueroquero.vtexassets.com/assets/vtex.file-manager-graphql/images/9ab2d4be-0913-4a93-bb23-0f407b34324d___95a0c9e10947f4f06c72dcbdad1cd104.svg' 
-                layout='responsive'
-                width={700} 
-                height={500}>
-                </Image>
             </div>
         </>
     )
